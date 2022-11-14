91adcee6d15cce2e264465bce9dc9bc00dd07da5
Salim Alam <salimalamdeveloper@gmail.com>
2022-11-14 10:44:53 +0530

    INFRA: Initialized unit test project

---
+++ Borshimar.Core.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,12 @@
+using Xunit;
+using FluentAssertions;
+
+namespace Borshimar.Core.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() =>
+                   true.Should().BeTrue();
+    }
+}
