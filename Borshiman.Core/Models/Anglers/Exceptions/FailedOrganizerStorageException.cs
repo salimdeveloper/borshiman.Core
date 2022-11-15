da753cd10cc5050b236b0d496fafd26453be8b8c
Salim Alam <salimalamdeveloper@gmail.com>
2022-11-15 12:06:48 +0530

    EXCEPTIONS: FailedAnglerStorageException Added

---
+++ Borshiman.Core/Models/Anglers/Exceptions/FailedOrganizerStorageException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xeptions;
+
+namespace Borshiman.Core.Models.Organizers.Exceptions
+{
+    public class FailedAnglerStorageException : Xeption
+    {
+        public FailedAnglerStorageException(Exception innerException)
+            : base("Failed angler storage error occured, contact support!",innerException)
+        {}
+    }
+}
