fa0795ae860baf9fd92862a3e29e06db115e2c3c
Salim Alam <salimalamdeveloper@gmail.com>
2022-11-15 12:05:51 +0530

    MODELS: Angler Model Added

---
+++ Borshiman.Core/Models/Anglers/Anglers.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System;
+
+namespace Borshiman.Core.Models
+{
+    public class Anglers
+    {
+        public long Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string Gender { get; set; }
+        public string PlayingHand { get; set; }
+        public Address Address { get; set; }
+        public long MobileNo { get; set; }
+        public bool MobileVerified { get; set; }
+        public string Email { get; set; }
+
+    }
+    public class Address
+    {
+        public string City { get; set; }
+        public int PinCode { get; set; }
+    }
+}
